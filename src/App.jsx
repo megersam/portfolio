5a644f05784e0e3346195f56656ea326ffa49e70
megersam <megersabmuleta01@gmail.com>
2025-01-31 18:07:23 +0300

    feat: add @vercel/analytics package and integrate Analytics component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
-
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
@@ -20,7 +20,9 @@ const App = () => {
           <StarsCanvas />
         </div>
       </div>
+      <Analytics />
     </BrowserRouter>
+    
   );
 }
 
